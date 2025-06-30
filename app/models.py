0136f2b35fc02c413e155d09c8250d6a62ac7eee
mlh <mha1555@wgu.edu>
2025-06-30 14:40:41 -0400

    Create the Pokemon class and make objects as a string for the models.py file

--- app/models.py
+++ app/models.py
@@ -2,5 +2,18 @@
 
 # Import modules
 from sqlalchemy import Column, Integer, String
-from .apiconfig import pokemon_name, pokemon_info
 from ..database import Base
+
+# Create Pokemon model
+class Pokemon(Base):
+  __tablename__ = 'pokemon'
+
+  # Define columns
+  id = Column(Integer, primary_key=True, index=True)
+  name = Column(String, nullable=False)
+  height = Column(Integer)
+  weight = Column(Integer)
+
+  # Set objects as readable strings
+  def __repr__(self):
+    return f"<Pokemon(id={self.id}, name='{self.name}', height={self.height}, weight={self.weight})>"
